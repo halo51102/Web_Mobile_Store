b326f87befccf0dd63aa907ba51bd0626786773f
halo51102 <20110667@student.hcmute.edu.vn>
2022-12-20 14:31:17 +0700

    page admin front-end

--- WebBanDienThoai/src/main/webapp/views/script.js
+++ WebBanDienThoai/src/main/webapp/views/script.js
@@ -1,6 +1,6 @@
 var slideIndex = 0;
 showSlides();
-
+ 
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
@@ -16,4 +16,45 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 2000); // Chuyển ảnh sau 2 giây
+}
+
+
+// ======================= Các Tab =========================
+function addEventChangeTab() {
+    var sidebar = document.getElementsByClassName('sidebar')[0];
+    var list_a = sidebar.getElementsByTagName('a');
+    for(var a of list_a) {
+        if(!a.onclick) {
+            a.addEventListener('click', function() {
+                turnOff_Active();
+                this.classList.add('active');
+                var tab = this.childNodes[1].data.trim();
+                openTab(tab);
+            })
+        }
+    }
+}
+
+function turnOff_Active() {
+    var sidebar = document.getElementsByClassName('sidebar')[0];
+    var list_a = sidebar.getElementsByTagName('a');
+    for(var a of list_a) {
+        a.classList.remove('active');
+    }
+}
+
+function openTab(nameTab) {
+    // ẩn hết
+    var main = document.getElementsByClassName('main')[0].children;
+    for(var e of main) {
+        e.style.display = 'none';
+    }
+
+    // mở tab
+    switch(nameTab) {
+        case 'Trang Chủ': document.getElementsByClassName('home')[0].style.display = 'block'; break;
+        case 'Sản Phẩm': document.getElementsByClassName('sanpham')[0].style.display = 'block'; break;
+        case 'Đơn Hàng': document.getElementsByClassName('donhang')[0].style.display = 'block'; break;
+        case 'Khách Hàng': document.getElementsByClassName('khachhang')[0].style.display = 'block'; break;
+    }
 }
